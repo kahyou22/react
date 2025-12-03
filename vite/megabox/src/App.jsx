69a5dbb7ae07db69a84d4d8231d5c13efab67875
kahyou22 <kahyou222@gmail.com>
2025-12-03 16:54:43 +0900

    메가박스

--- vite/megabox/src/App.jsx
+++ vite/megabox/src/App.jsx
@@ -1,5 +1,4 @@
-import "./App.css";
-import "./../public/css/reset.css";
+import "./css/reset.css";
 import BoxOfficeCard from "./BoxOfficeCard";
 import { datas } from "./data";
 
@@ -7,7 +6,7 @@ function App() {
   return (
     <div className="box_office">
       {datas.map((data) => (
-        <BoxOfficeCard data={data} key={data} />
+        <BoxOfficeCard data={data} key={data.title} />
       ))}
     </div>
   );
--- vite/megabox/src/BoxOfficeCard.jsx
+++ vite/megabox/src/BoxOfficeCard.jsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
+
+import "./css/BoxOfficeCard.css";
+
 function BoxOfficeCard({ data }) {
-  // const [liked, setLiked] = React.useState(false);
-  // const [count, setCount] = React.useState(360);
+  const [liked, setLiked] = useState(false);
+  const [count, setCount] = useState(data.like);
 
   // // k 단위 변환
-  // const formatCount = (num) => {
-  //   return num >= 1000 ? (num / 1000).toFixed(1) + "k" : num;
-  // };
+  const formatCount = (num) => {
+    return num >= 1000 ? (num / 1000).toFixed(1) + "k" : num;
+  };
 
-  // const handleClick = () => {
-  //   const newLiked = !liked;
-  //   const newCount = newLiked ? count + 1 : count - 1;
-  //   setLiked(newLiked);
-  //   setCount(newCount);
-  // };
+  const handleClick = () => {
+    const newLiked = !liked;
+    const newCount = newLiked ? count + 1 : count - 1;
+    setLiked(newLiked);
+    setCount(newCount);
+  };
 
   return (
     <div className="box_office_card">
@@ -39,9 +43,9 @@ function BoxOfficeCard({ data }) {
         </div>
       </div>
       <div className="btn">
-        <button type="button" className={`like`}>
-          <i className={"fa-regular fa-heart"}></i>
-          {data.like}
+        <button type="button" className={`like`} onClick={handleClick}>
+          <i className={(liked ? "fa-solid" : "fa-regular") + " fa-heart"}></i>
+          {formatCount(count)}
         </button>
         <a
           href="https://www.megabox.co.kr/booking"
